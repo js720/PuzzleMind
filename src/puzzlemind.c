8cb46496ae6c7e3c53e2fb2d28a11f0edf705174
JS427 <itsjonghyunshin427@gmail.com>
2022-07-07 07:18:19 +0900

    Create puzzlemind.c

---
+++ src/puzzlemind.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+
+int main()
+{
+          int answer;
+          printf("Whats 5+5?\n");
+          scanf("%d", &answer);
+          if (answer != 10)
+          {
+                    printf("Incorrect.:(\n");
+          }
+          else
+          {
+                    printf("Correct!\n");
+                    printf("Whats 10+5?\n");
+                    scanf("%d", &answer);
+                    if (answer != 15)
+                    {
+                              printf("Incorrect.:(\n");
+                    }
+                    else
+                    {
+                              printf("Correct!\n");
+                              printf("Whats 7+7?\n");
+                              scanf("%d", &answer);
+                              if (answer != 14)
+                              {
+                                        printf("Incorrect.:(\n");
+                              }
+                              else
+                              {
+                                        printf("Correct!\n");
+                                        printf("Whats 6+7?\n");
+                                        scanf("%d", &answer);
+                                        if (answer != 13)
+                                        {
+                                                  printf("Incorrect.:(\n");
+                                        }
+                                        else
+                                        {
+                                                  printf("Correct!\n");
+                                                  printf("Whats 5+3?\n");
+                                                  scanf("%d", &answer);
+                                                  if (answer != 8)
+                                                  {
+                                                            printf("Incorrect.:(\n");
+                                                  }
+                                                  else
+                                                  {
+                                                            printf("Correct!\n");
+                                                            printf("Whats 9+9?\n");
+                                                            scanf("%d", &answer);
+                                                            if (answer != 18)
+                                                            {
+                                                                      printf("Incorrect.:(\n");
+                                                            }
+                                                            else
+                                                            {
+                                                                      printf("Correct!\n");
+                                                                      printf("Whats 8+8?\n");
+                                                                      scanf("%d", &answer);
+                                                                      if (answer != 16)
+                                                                      {
+                                                                                printf("Incorrect.:(\n");
+                                                                      }
+                                                                      else
+                                                                      {
+                                                                                printf("Correct!\n");
+                                                                                printf("Whats 7+5?\n");
+                                                                                scanf("%d", &answer);
+                                                                                if (answer != 12)
+                                                                                {
+                                                                                          printf("Incorrect.:(\n");
+                                                                                }
+                                                                                else
+                                                                                {
+                                                                                          printf("Correct!\n");
+                                                                                          printf("Whats 9+8?\n");
+                                                                                          scanf("%d", &answer);
+                                                                                          if (answer != 17)
+                                                                                          {
+                                                                                                    printf("Incorrect.:(\n");
+                                                                                          }
+                                                                                          else
+                                                                                          {
+                                                                                                    printf("Correct!\n");
+                                                                                                    printf("Whats 8+5?\n");
+                                                                                                    scanf("%d", &answer);
+                                                                                                    if (answer != 13)
+                                                                                                    {
+                                                                                                              printf("Incorrect.:(");
+                                                                                                    }
+                                                                                          }
+                                                                                }
+                                                                      }
+                                                            }
+                                                  }
+                                        }
+                              }
+                    }
+          }
+          return 0;
+}
